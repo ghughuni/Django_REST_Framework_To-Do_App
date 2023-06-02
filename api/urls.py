b66c33a397aadb59adfa4772499df6e08bd3bc01
main <gkikvidze91@gmail.com>
2023-06-02 19:36:50 +0400

    create Serializers and CRUD

---
+++ api/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import (
+    apiOverview,
+    taskList,
+    taskDetail,
+    taskCreate,
+    taskUpdate,
+    taskDelete
+
+)
+
+urlpatterns = [
+	path('', apiOverview, name="api-overview"),
+	path('task-list/', taskList, name="task-list"),
+	path('task-detail/<str:pk>/', taskDetail, name="task-detail"),
+	path('task-create/', taskCreate, name="task-create"),
+	path('task-update/<str:pk>/', taskUpdate, name="task-update"),
+	path('task-delete/<str:pk>/', taskDelete, name="task-delete"),
+]
