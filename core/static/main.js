28cfc141cd12615d52607fc9a631e194f5aa8f93
main <gkikvidze91@gmail.com>
2023-06-03 17:43:18 +0400

    Create new task on form with 'add'

--- core/static/main.js
+++ core/static/main.js
@@ -1,7 +1,25 @@
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+const csrftoken = getCookie("csrftoken");
+
 show_list();
 
 function show_list() {
   const wrapper = document.getElementById("list-wrapper");
+  wrapper.innerHTML = "";
   const url = "http://127.0.0.1:8000/api/task-list/";
 
   fetch(url)
@@ -17,8 +35,8 @@ function show_list() {
                         <div class="title">${list[i].title}</div>
                     </div>
                     <div class="d-flex justify-content-end align-items-center">
-                        <button class="btn btn-sm btn-outline-info edit">Edit</button>
-                        <button class="btn btn-sm btn-outline-danger delete"><i class="bi bi-trash-fill"></i></button>
+                        <button class="btn btn-sm btn-outline-info edit mx-1">Edit</button>
+                        <button class="btn btn-sm btn-outline-danger delete mx-1"><i class="bi bi-trash-fill"></i></button>
                     </div>
                 </div>
                 `;
@@ -27,3 +45,21 @@ function show_list() {
       }
     });
 }
+const form = document.getElementById("form-wrapper");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("Form added");
+  const url = "http://127.0.0.1:8000/api/task-create/";
+  const title = document.getElementById("title").value;
+  fetch(url, {
+    method: `POST`,
+    headers: {
+      "Content-type": "application/json",
+      "X-CSRFTToken": csrftoken,
+    },
+    body: JSON.stringify({ title: title }),
+  }).then(function (response) {
+    show_list();
+    document.getElementById("form").reset();
+  });
+});
